80bebaa378793add3e068fd037023c9f68178487
Akshayashetty123 <140807640+Akshayashetty123@users.noreply.github.com>
2023-08-15 20:01:19 +0530

    Initial Commit

---
+++ BinaryToDec.java
@@ -0,0 +1,24 @@
+public class BinaryToDec{
+  public static void main(String[] args){
+    System.out.println("Enter a Number");
+    int bin=5;
+    int temp=bin;
+    int count=0;
+    double sum=0;
+    while(bin!=0){
+      int d=bin%10;
+      if(d>1){
+        count=-1;
+        break;
+      }
+      count++;
+      sum=sum+(d*Math.pow(2,count));
+      bin=bin/10;
+    }
+    if(count==-1)
+      System.out.println(temp+" is not a binary number");
+    else
+      System.out.println((int)sum/2);
+    sc.close();
+  }
+}
